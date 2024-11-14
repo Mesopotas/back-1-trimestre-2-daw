1993103e398a5290c8d7eb972e2572a35e3642b1
Nicolas <n.abuin2005@gmail.com>
2024-11-14 21:09:45 +0100

    commit obligatorio por Developer

--- obj/Debug/net8.0/back-1-trimestre-2-daw.AssemblyInfo.cs
+++ obj/Debug/net8.0/back-1-trimestre-2-daw.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-1-trimestre-2-daw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c5cae28cd93290b89c523e8fb6d7854bf9182fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+381b7313f4513d49cd18256bb0a3bd72140ac35c")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-1-trimestre-2-daw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-1-trimestre-2-daw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
