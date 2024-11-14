ba9e3c6e79a52f6bdb21369da9b54dd218f36412
Nicolas <n.abuin2005@gmail.com>
2024-11-14 20:40:55 +0100

    Actualizacion del gitignore y eliminacion de wheaterForecast

--- obj/Debug/net8.0/back-1-trimestre-2-daw.AssemblyInfo.cs
+++ obj/Debug/net8.0/back-1-trimestre-2-daw.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-1-trimestre-2-daw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd8a659694dd6251976ba2a52cd3ead0b8955fb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a68c76079b1c0e342c82d47a8894d7f93fa420")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-1-trimestre-2-daw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-1-trimestre-2-daw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
