010969d1cbfab9e60deb31a02ebf9d490892f4d8
Nicolas <n.abuin2005@gmail.com>
2024-11-21 12:27:28 +0100

    commit preMerge de Controllers a Developer

--- obj/Debug/net8.0/back-1-trimestre-2-daw.AssemblyInfo.cs
+++ obj/Debug/net8.0/back-1-trimestre-2-daw.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-1-trimestre-2-daw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c8f4d0221efd20cbdb698be6277c6a8d03bf9de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ddcdf8a6da44351a27ebf0b971efad453faa1b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-1-trimestre-2-daw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-1-trimestre-2-daw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
