6efff57e94e50e7bd4b27a37becafe3ec59581ef
Nicolas <n.abuin2005@gmail.com>
2024-11-26 17:20:31 +0100

    commit pre merge

--- obj/Debug/net8.0/back-1-trimestre-2-daw.AssemblyInfo.cs
+++ obj/Debug/net8.0/back-1-trimestre-2-daw.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-1-trimestre-2-daw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21cb6e0ce0806d093d66c38900d2e1960244449")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1097a7a6e7369b47f3662abbf9714abef8e0ef54")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-1-trimestre-2-daw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-1-trimestre-2-daw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
